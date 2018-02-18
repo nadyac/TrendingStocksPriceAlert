43fa4eb087d8b1f78875159ca5914428b2fb9720
nadyac <nadyacpena@gmail.com>
2018-02-18 16:08:49 -0500

    Made twilio sms parameters dynamic based on the get request path.

--- scripts/python/SMS.py
+++ scripts/python/SMS.py
@@ -1,12 +1,19 @@
 from twilio.rest import Client
 
-def process(path):
-	print(path)
+def getSymbol(path):
 	symbol = path[len("/setAlert?alertSymbol="):-len("&toNumber=12223334444")]
 	return symbol
+	
+def getPhoneNumber(path):
+	splitPath = path.split("&")
+	phoneNumberParam = splitPath[1]
+	phoneNumber = phoneNumberParam.split("=")[1]
+	return phoneNumber
 
-def setAlert(params):
-	targetSymbol = process(params)
+def setAlert(pathWithParams):
+	targetSymbol = getSymbol(pathWithParams)
+	toNumber = getPhoneNumber(pathWithParams)
+
 	# Your Account SID from twilio.com/console
 	account_sid = "AC805fbc5f452cd5a175c2df46b07b70f4"
 	# Your Auth Token from twilio.com/console
@@ -15,7 +22,7 @@ def setAlert(params):
 	client = Client(account_sid, auth_token)
 
 	message = client.messages.create(
-	    to="+12019262482", #TODO make this dynamic
+	    to="+1"+toNumber, #TODO make this dynamic
 	    from_="+12015966491",
 	    body="You have set up a price alert for " + targetSymbol +"!")
 
